a5a51a0b5f5f1b9466dc0a4aba760db1bdadc3c1
eduardosand <esandov7@gmail.com>
2024-05-15 11:47:12 -0700

    low signal to noise photodiode script, so altering how we choose timepoints

--- scripts/A_timestamps_start_stop.py
+++ scripts/A_timestamps_start_stop.py
@@ -1,4 +1,5 @@
-from intracranial_ephys_utils.manual_process import reformat_event_labels, photodiode_check_viewer
+from intracranial_ephys_utils.manual_process import (reformat_event_labels, photodiode_check_viewer,
+                                                     diagnostic_time_series_plot)
 from pathlib import Path
 import os
 
@@ -7,12 +8,20 @@ import os
 # and mark down when the wcst experiment was, with the idea that when the photodiode is still, the data should largely
 # have similar statistics, and make it easier to do photodiode processing (trial detection).
 
-subject = 'IR87'
-session = 'sess-4'
+# subject = 'IR87'
+# session = 'sess-4'
+subject = 'IR95'
+session = 'sess-3'
 task = 'wcst'
 sort_directory = Path(f"{os.pardir}/data/{subject}/{session}/")
 
 data_directory = Path(f"{os.pardir}/data/{subject}/{session}/raw")
 
 reformat_event_labels(subject, session, task, data_directory, sort_directory)
-photodiode_check_viewer(subject, session, task, data_directory, sort_directory)
+#######
+# There is an issue when photodiode signal is noisy, which is that this entire pipeline doesn't make a lot of sense
+# anymore. In these cases, we'd like to process the photodiode signal if possible. However, if it's noisy there might
+# some weird non-linearity(due to removing the photodiode from the screen in between sessions etc). So what we will do
+# is first plot the entire photodiode signal at a glance to remove some of this, hoping that this doesn't interfere
+# with annotations (as long as it's towards the end of the dataset it's fine).
+photodiode_check_viewer(subject, session, task, data_directory, sort_directory, diagnostic=True)
