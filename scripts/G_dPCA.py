36145cf49e45529132b5f1039c2d22a8ef912b88
eduardosand <esandov7@gmail.com>
2024-06-02 15:57:59 -0700

    Two different ways of doing PCA and comparing to dPCA

--- scripts/G_dPCA.py
+++ scripts/G_dPCA.py
@@ -129,6 +129,8 @@ def plot_signal_avg(organized_data_mean, subject, session, trial_time,
         ]
     for ind, ax_curr in enumerate(ax.flatten()):
         if ind >= n_electrodes:
+            ax.flatten()[ind-2].set_xlabel('Time (s)')
+            ax.flatten()[ind-2].set_xticks(time_ticks, time_tick_labels)
             continue
         for cond in range(n_cond):
             ax_curr.plot(trial_time, organized_data_mean[ind, cond], label=labels[cond], color=color_dict[labels[cond]])
@@ -253,7 +255,7 @@ def plot_dPCA_components(dpca, Z, trial_time, features, subject, session, suptit
     plt.show()
 
 
-def pca_comparison(dpca, organized_data_mean):
+def pca_comparison(dpca, organized_data_mean , type):
     """
     My intuition with dPCA is that the components should be similar to normal PCA components but with the added
     benefit of labels. In this case the cumulative variance explained by PCA and dPCA should be comparable.
@@ -272,141 +274,208 @@ def pca_comparison(dpca, organized_data_mean):
     plt.plot(np.cumsum(dpca_explained_var_ratios), label='dPCA')
     plt.xlabel('Number of components')
     plt.ylabel('Cumulative variance explained')
-    plt.title('PCA vs. dPCA')
+    plt.title(f'PCA - {type} vs. dPCA')
     plt.legend()
     plt.show()
 
+
 # This script serves to perform a dPCA on the spiking data
+def sua_prep(subject, session, standardized_data=False, event_lock='Onset'):
+    timestamps_file = f"sub-{subject}-{session}-ph_timestamps.csv"
 
-# the first step towards that aim is to get my neural data and plot it
+    data_directory = Path(f'{os.pardir}/data/{subject}/{session}')
+    ph_file_path = get_file_info(data_directory / Path('raw'), 'photo1', '.ncs')
 
-session = 'sess-3'
+
+    running_avg = 5
+    bhv_directory = data_directory / Path("behavior")
+    bhv_file_path = get_file_info(bhv_directory, f'{subject}', '.csv')
+
+    beh_data, rule_shifts_ind, _ = process_wcst_behavior(bhv_file_path,
+                                                         running_avg=running_avg)
+
+
+    beh_data.set_index(['trial'], inplace=True)
+    beh_timestamps = pd.read_csv(bhv_directory / timestamps_file)
+    feedback_times = beh_timestamps['Feedback (seconds)']
+    onset_times = beh_timestamps['Onset (seconds)']
+    if event_lock == 'Onset':
+        event_times = onset_times
+    elif event_lock == 'Feedback':
+        event_times = feedback_times
+
+
+    # global t-start
+    reader = read_file(ph_file_path)
+    reader.parse_header()
+    start_record = reader.global_t_start
+    number_spikes = []
+    curr_neuron = 0
+
+    su_data_dir = data_directory / "sorted/sort/final"
+    all_su_files = os.listdir(su_data_dir)
+
+    neuron_count = 0
+    for file in all_su_files:
+        data = loadmat(os.path.join(su_data_dir, file))
+        # print(file)
+        # print(data['useNegative'])
+        # print(data['useNegative'].shape)
+        neuron_count += data['useNegative'][0].shape[0]
+
+    file = all_su_files[0]
+    max_rt = max(beh_data['response_time'])
+
+    # ahead of grabbing neural data, pre-define some things that are true for every neuron
+    binsize = 0.01
+    step = 0.01
+    tmin_onset = -1
+        # tmax = max_rt+1.5
+    tmax = 1.5
+    trial_time = np.arange(tmin_onset, tmax + step, step)
+    n_trials = len(beh_data['correct'])
+    n_timepoints = trial_time.shape[0]
+    neural_data = np.zeros((n_trials, neuron_count, n_timepoints))
+    labels = []
+
+    curr_neur = 0
+    for file in all_su_files:
+
+        # step 1. Load in one file, and comb through it for neurons
+        microwire_spikes = loadmat(su_data_dir / file)
+        neuron_counts = microwire_spikes['useNegative'][0].shape[0]
+
+
+        for neuron_ind in range(neuron_counts):
+            su_cluster_num = microwire_spikes['useNegative'][0][neuron_ind]
+            labels.append(str(su_cluster_num))
+            # Note that these timestamps are in microseconds, and according to machine clock
+            microsec_sec_trans = 10**-6
+            su_timestamps = np.array([[microwire_spikes['newTimestampsNegative'][0, i]*microsec_sec_trans-start_record] for i in
+                                          range(microwire_spikes['newTimestampsNegative'].shape[1])
+                                         if microwire_spikes['assignedNegative'][0, i] == su_cluster_num])
+
+            # trial_wise_feedback_spikes = get_trial_wise_times(su_timestamps, feedback_times, beh_data, tmin=-1., tmax=1.5)
+            # Plot response in spikes of this one neuron relative to each onset event
+            trial_wise_feedback_spikes = get_trial_wise_times(su_timestamps, event_times, tmin=tmin_onset, tmax=tmax)
+
+            sort_order = sorted(set(beh_data['correct']))
+            if len(sort_order) == 3:
+                color_dict = dict(zip(sort_order, ['red', 'green', 'blue']))
+            else:
+                color_dict = dict(zip(sort_order, ['purple', 'orange']))
+
+            spike_trains_sorted, beh_conditions_sorted, change_indices = get_spike_rate_curves(trial_wise_feedback_spikes,
+                                                                                               beh_data['correct'],
+                                                                                               tmin=tmin_onset,
+                                                                                               tmax=tmax, step=step,
+                                                                                               binsize=binsize,
+                                                                                               mode='single_trial')
+
+            sigma = 0.05
+            filtered_signals = gaussian_smooth(spike_trains_sorted, sigma, step)
+            neural_data[:, curr_neur, :] = filtered_signals
+            curr_neur += 1
+            # fig, axs = plt.subplots(nrows=2)
+            # plot_neural_spike_trains(axs[0], trial_wise_feedback_spikes, beh_data['correct'], color_dict)
+            # axs[0].set_xlim([tmin_onset, tmax])
+            # for i in range(filtered_signals.shape[0]):
+            #     axs[1].plot(trial_time, filtered_signals[i, :], color=color_dict[beh_conditions_sorted[i]])
+            # axs[0].set_title(f"Comparing raster plots and smoothed curves for {su_cluster_num}")
+            # axs[1].set_xlabel("Time (s)")
+            # axs[1].set_xlim([tmin_onset, tmax])
+            # # axs[0].axis('off')
+            # plt.show()
+
+
+    # labels are labels for the single units, consider using the cluster number
+    # for ch type, don't use seeg
+    eff_fs = int(1/step)
+    mne_info = mne.create_info(list(labels), eff_fs, ch_types='seeg')
+
+    # su_data is just what we're plotting above
+    epochs_object = mne.EpochsArray(neural_data, mne_info)
+
+    # might take some time to featurize the objects appropriately
+    # baselining is a problem
+    organized_data_mean, organized_data, feedback_dict = featurize(epochs_object, beh_conditions_sorted,
+                                                                   norm=standardized_data)
+    return organized_data_mean, organized_data, feedback_dict, trial_time
+
+session = 'sess-2'
 subject = 'IR95'
-timestamps_file = f"sub-{subject}-{session}-ph_timestamps.csv"
-
-data_directory = Path(f'{os.pardir}/data/{subject}/{session}')
-ph_file_path = get_file_info(data_directory / Path('raw'), 'photo1', '.ncs')
-
-
-running_avg = 5
-bhv_directory = data_directory / Path("behavior")
-bhv_file_path = get_file_info(bhv_directory, f'{subject}', '.csv')
-
-beh_data, rule_shifts_ind, _ = process_wcst_behavior(bhv_file_path,
-                                                     running_avg=running_avg)
-
-
-beh_data.set_index(['trial'], inplace=True)
-beh_timestamps = pd.read_csv(bhv_directory / timestamps_file)
-feedback_times = beh_timestamps['Feedback (seconds)']
-onset_times = beh_timestamps['Onset (seconds)']
-
-
-# global t-start
-reader = read_file(ph_file_path)
-reader.parse_header()
-start_record = reader.global_t_start
-number_spikes = []
-curr_neuron = 0
-
-su_data_dir = data_directory / "sorted/sort/final"
-all_su_files = os.listdir(su_data_dir)
-
-neuron_count = 0
-for file in all_su_files:
-    data = loadmat(os.path.join(su_data_dir, file))
-    # print(file)
-    # print(data['useNegative'])
-    # print(data['useNegative'].shape)
-    neuron_count += data['useNegative'][0].shape[0]
-
-file = all_su_files[0]
-max_rt = max(beh_data['response_time'])
-
-# ahead of grabbing neural data, pre-define some things that are true for every neuron
-binsize = 0.01
-step = 0.01
-tmin_onset = -1
-    # tmax = max_rt+1.5
-tmax = 1.5
-trial_time = np.arange(tmin_onset, tmax + step, step)
-n_trials = len(beh_data['correct'])
-n_timepoints = trial_time.shape[0]
-neural_data = np.zeros((n_trials, neuron_count, n_timepoints))
-labels = []
-
-curr_neur = 0
-for file in all_su_files:
-
-    # step 1. Load in one file, and comb through it for neurons
-    microwire_spikes = loadmat(su_data_dir / file)
-    neuron_counts = microwire_spikes['useNegative'][0].shape[0]
-
-
-    for neuron_ind in range(neuron_counts):
-        su_cluster_num = microwire_spikes['useNegative'][0][neuron_ind]
-        labels.append(str(su_cluster_num))
-        # Note that these timestamps are in microseconds, and according to machine clock
-        microsec_sec_trans = 10**-6
-        su_timestamps = np.array([[microwire_spikes['newTimestampsNegative'][0, i]*microsec_sec_trans-start_record] for i in
-                                      range(microwire_spikes['newTimestampsNegative'].shape[1])
-                                     if microwire_spikes['assignedNegative'][0, i] == su_cluster_num])
-
-        # trial_wise_feedback_spikes = get_trial_wise_times(su_timestamps, feedback_times, beh_data, tmin=-1., tmax=1.5)
-        # Plot response in spikes of this one neuron relative to each onset event
-        trial_wise_feedback_spikes = get_trial_wise_times(su_timestamps, feedback_times, tmin=tmin_onset, tmax=tmax)
-
-        sort_order = sorted(set(beh_data['correct']))
-        if len(sort_order) == 3:
-            color_dict = dict(zip(sort_order, ['red', 'green', 'blue']))
-        else:
-            color_dict = dict(zip(sort_order, ['purple', 'orange']))
-
-        spike_trains_sorted, beh_conditions_sorted, change_indices = get_spike_rate_curves(trial_wise_feedback_spikes,
-                                                                                           beh_data['correct'],
-                                                                                           tmin=tmin_onset,
-                                                                                           tmax=tmax, step=step,
-                                                                                           binsize=binsize,
-                                                                                           mode='single_trial')
-
-        sigma = 0.05
-        filtered_signals = gaussian_smooth(spike_trains_sorted, sigma, step)
-        neural_data[:, curr_neur, :] = filtered_signals
-        curr_neur += 1
-        fig, axs = plt.subplots(nrows=2)
-        plot_neural_spike_trains(axs[0], trial_wise_feedback_spikes, beh_data['correct'], color_dict)
-        axs[0].set_xlim([tmin_onset, tmax])
-        for i in range(filtered_signals.shape[0]):
-            axs[1].plot(trial_time, filtered_signals[i, :], color=color_dict[beh_conditions_sorted[i]])
-        axs[0].set_title(f"Comparing raster plots and smoothed curves for {su_cluster_num}")
-        axs[1].set_xlabel("Time (s)")
-        axs[1].set_xlim([tmin_onset, tmax])
-        # axs[0].axis('off')
-        plt.show()
-
-
-# labels are labels for the single units, consider using the cluster number
-# for ch type, don't use seeg
-eff_fs = int(1/step)
-mne_info = mne.create_info(list(labels), eff_fs, ch_types='seeg')
-standardized_data = True
-
-# su_data is just what we're plotting above
-epochs_object = mne.EpochsArray(neural_data, mne_info)
-
-# might take some time to featurize the objects appropriately
-# baselining is a problem
-organized_data_mean, organized_data, feedback_dict = featurize(epochs_object, beh_conditions_sorted,
-                                                               norm=standardized_data)
+standardized_data = False
+event_lock = 'Onset'
+organized_data_mean, organized_data, feedback_dict, trial_time = sua_prep(subject, session, standardized_data,
+                                                                          event_lock)
 plot_signal_avg(organized_data_mean, subject, session, trial_time, labels=feedback_dict,
-                extra_string=f'Normalization = {standardized_data}')
-
-dpca = dPCA.dPCA(labels='st', regularizer='auto')
+                extra_string=f'Normalization = {standardized_data} {event_lock}-lock')
+regularization_setting = None
+# regularization_setting = 'auto'
+dpca = dPCA.dPCA(labels='st', regularizer=regularization_setting)
 dpca.protect = ['t']
 Z = dpca.fit_transform(organized_data_mean, organized_data)
 
-pca_comparison(dpca, organized_data_mean)
-suptitle = 'All single units'
+pca_comparison(dpca, organized_data_mean, type='trial average')
+new_organized_data = np.swapaxes(organized_data, 0, 1)
+new_organized_data_frame = pd.DataFrame(new_organized_data.reshape(new_organized_data.shape[0], -1))
+new_organized_data_frame.dropna(inplace=True, axis='columns')
+trial_wise_data_for_PCA = new_organized_data_frame.to_numpy()
+pca_comparison(dpca, trial_wise_data_for_PCA, type='trial concatenated')
+suptitle = f'All single units {event_lock}-locked'
+plot_dPCA_components(dpca, Z, trial_time, feedback_dict, subject, session, suptitle,
+                     feature_names=['feedback'])
+
+session3 = 'sess-3'
+organized_data_mean_3, organized_data_3, feedback_dict_3, trial_time_3 = sua_prep(subject, session3, standardized_data,
+                                                                                  event_lock)
+plot_signal_avg(organized_data_mean_3, subject, session3, trial_time, labels=feedback_dict,
+                extra_string=f'Normalization = {standardized_data}, {event_lock}-locked')
+
+dpca = dPCA.dPCA(labels='st', regularizer=regularization_setting)
+dpca.protect = ['t']
+Z_2 = dpca.fit_transform(organized_data_mean_3, organized_data_3)
+
+pca_comparison(dpca, organized_data_mean_3, type='trial average')
+new_organized_data = np.swapaxes(organized_data_mean_3, 0, 1)
+new_organized_data_frame = pd.DataFrame(new_organized_data.reshape(new_organized_data.shape[0], -1))
+new_organized_data_frame.dropna(inplace=True, axis='columns')
+trial_wise_data_for_PCA = new_organized_data_frame.to_numpy()
+pca_comparison(dpca, trial_wise_data_for_PCA, type='trial concatenated')
+suptitle = f'All single units {event_lock}-locked'
+plot_dPCA_components(dpca, Z_2, trial_time, feedback_dict, subject, session3, suptitle,
+                     feature_names=['feedback'])
+
+num_trials_2, num_neurons_2, num_cond, num_timepoints = organized_data.shape
+num_trials_3, num_neurons_3, _, _ = organized_data_3.shape
+total_neurons = num_neurons_2+num_neurons_3
+max_trials_per_cond = np.max([num_trials_2, num_trials_3])
+completed_data_set_mean = np.zeros((total_neurons,
+                                    num_cond, num_timepoints))
+completed_data_set_mean[completed_data_set_mean == 0] = np.nan
+completed_data_set_mean[0:organized_data_mean.shape[0], :, :] = organized_data_mean
+completed_data_set_mean[organized_data_mean.shape[0]:, :, :] = organized_data_mean_3
+
+
+completed_data_set = np.zeros((max_trials_per_cond, total_neurons,
+                                    num_cond, num_timepoints))
+completed_data_set[completed_data_set == 0] = np.nan
+completed_data_set[0:num_trials_2, 0:num_neurons_2, :, :] = organized_data
+completed_data_set[0:num_trials_3, num_neurons_2:, :, :] = organized_data_3
+
+
+dpca = dPCA.dPCA(labels='st', regularizer=regularization_setting)
+dpca.protect = ['t']
+Z = dpca.fit_transform(completed_data_set_mean, completed_data_set)
+
+pca_comparison(dpca, completed_data_set_mean, type='trial average')
+new_organized_data = np.swapaxes(completed_data_set_mean, 0, 1)
+new_organized_data_frame = pd.DataFrame(new_organized_data.reshape(new_organized_data.shape[0], -1))
+new_organized_data_frame.dropna(inplace=True, axis='columns')
+trial_wise_data_for_PCA = new_organized_data_frame.to_numpy()
+pca_comparison(dpca, trial_wise_data_for_PCA, type='trial concatenated')
+suptitle = f'All single units {event_lock}-locked'
+session = 'sess-2 and sess-3'
 plot_dPCA_components(dpca, Z, trial_time, feedback_dict, subject, session, suptitle,
                      feature_names=['feedback'])
 print('huh')
