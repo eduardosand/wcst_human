45296a0046e28c41dc61cbc5a44a9bb1e1fc995d
eduardosand <esandov7@gmail.com>
2024-09-18 14:43:52 -0700

    added stimulus data, no more information from the behavior needs to be computed for modelling purposes (crossed fingers)

--- scripts/bhv_data_convert.py
+++ scripts/bhv_data_convert.py
@@ -14,10 +14,12 @@ def bhv_convert(subject, session, intercept, save_directory):
     :param subject: (string) subject name
     :param session: (string) session name
     :param intercept: (int) probably 1 or 0
-    :return: history_data: (array) n_trials X n_features X n_history_features
-    :return: choice_data: (array) n_trials X n_features
+    :return: history_data: (array) n_trials X n_features X n_history_features - tells you the outcome for each trial
+    and feature.
+    :return: choice_data: (array) n_trials X n_features - tells us on a given trial whether a feature was chosen
     :return: history_possibilities: label each part(technically feature, but not the same feature as in the task) of the
-    history data
+    history data, specifically labels each index in the n_history_features to tell you what it means
+    :return object_choice_data: (array) n_cards X n_trials - tells you which card was chosen
     """
 
     data_directory = Path(f"{os.pardir}/data/{subject}/{session}/")
@@ -50,6 +52,7 @@ def bhv_convert(subject, session, intercept, save_directory):
     history_data = np.zeros((len(beh_data.index.values), len(features), len(history_possibilities)))
     choice_data = np.zeros((len(beh_data.index.values), len(features), 1), dtype=np.int8) # feature level
     object_choice_data = np.zeros((len(locations), len(beh_data.index.values)), dtype=np.int8)
+    stimulus_data = np.zeros((len(features), len(locations), len(beh_data.index.values)))
     for ind, row in enumerate(beh_data.index.values):
         # rule = beh_data.loc[row, 'rule']
         # connect this to what 'card' was correct
@@ -64,8 +67,20 @@ def bhv_convert(subject, session, intercept, save_directory):
         # do this across trials
         choice_data[ind, chosen_ind] = 1.
 
+        # collect stimuli for this trial on an object level
+        object_stimuli = [beh_data.loc[row, f'bmp_table_{i+1}'][:-4] for i in range(4)]
         # for each feature and trial, we need to see if it was chosen and rewarded
         for feature_ind, feature in enumerate(features):
+            # to update stimulus data first find the card associated with that feature
+            if feature in ['S0', 'S2']:
+                # looking for S in two different spots
+                card_num = [i for i in range(len(object_stimuli)) if feature[0] in object_stimuli[i][int(feature[1])]][0]
+                stimulus_data[feature_ind, card_num, ind] = 1.
+            else:
+                # looking for feature in card
+                card_num = [i for i in range(len(object_stimuli)) if feature in object_stimuli[i]][0]
+                stimulus_data[feature_ind, card_num, ind] = 1.
+
             # look for feature in chosen card, but problem if feature is S0 or S2
             if feature.startswith('S'):
                 if 'S' in chosen_card:
@@ -80,6 +95,7 @@ def bhv_convert(subject, session, intercept, save_directory):
                 chosen = 1
             else:
                 chosen = 0
+
             feature_history = history_choice_dict[(reward, chosen)]
             history_data[ind, feature_ind, history_possibilities.index(feature_history)] = 1.
         # for location in list(keys_img_location_dict.values()):
@@ -105,14 +121,19 @@ def bhv_convert(subject, session, intercept, save_directory):
     history_data[:, :, 0] = intercept
     # Final step, drop the first trial because there's no history of choice from the last trial
     choice_data = choice_data[1:, :, :]
+    object_choice_data = object_choice_data[:, 1:]
+    stimulus_data = stimulus_data[:, :, 1:]
     # and then drop the last trial for X because it won't predict the choice on the next trial (the next trial doesn't exist)
     history_data = history_data[:-1, :, :]
+
+
+
     # this effectively shifts the data so that history_data[ind,:,:] is always the information from trial ind-1
     lag = 1
 
     # save pickle file so this code doesn't need to run
     save_name = (f'{subject}_{session}_glm1_lag{lag}_int{intercept}.pickle')
     with open(save_directory / save_name, 'wb') as f:
-        pickle.dump([history_data, choice_data, history_possibilities, object_choice_data], f)
-    return history_data, choice_data, history_possibilities, object_choice_data
+        pickle.dump([history_data, choice_data, history_possibilities, object_choice_data, locations, stimulus_data], f)
+    return history_data, choice_data, history_possibilities, object_choice_data, locations, stimulus_data
 
