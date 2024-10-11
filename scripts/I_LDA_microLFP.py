5680d9bfc9198b4780d696d623e36f9dfdc459db
eduardosand <esandov7@gmail.com>
2024-10-11 11:14:29 -0700

    looking at loadings of LDA

--- scripts/I_LDA_microLFP.py
+++ scripts/I_LDA_microLFP.py
@@ -18,7 +18,7 @@ feature = 'correct'
 # feature = 'rule dimension'
 standardized_data = True
 # standardized_data = False
-electrode_selection = 'macrocontact'
+electrode_selection = 'all'
 # baseline=(2,2.5)
 baseline = (-0.5, 0)
 car_setting = False
@@ -113,9 +113,13 @@ extra_string_start = 'normalized' + ', bp and notch filtered'
 if car_setting:
     extra_string = f'{extra_string_start} {event_lock}-lock \n corrected for multiple comparisons \n CAR'
 else:
-    extra_string = f'{extra_string_start} {event_lock}-lock \n corrected for multiple comparisons \n no CAR'
-plt.suptitle(f'Mean Accuracy of LDA on {electrode_selection} broadband LFP \n {test_subject} - session {test_session} '
-             f'\n {extra_string}')
+    extra_string = f'{extra_string_start} {event_lock}-lock \n corrected for multiple comparisons'
+# plt.suptitle(f'Mean Accuracy of LDA on {electrode_selection} broadband LFP \n {test_subject} - session {test_session} '
+#              f'\n {extra_string}')
+if electrode_selection == 'all':
+    plt.suptitle(f'LDA Accuracy on \n {electrode_selection} broadband LFP', fontsize=26)
+else:
+    plt.suptitle(f'LDA Accuracy on \n {electrode_selection} broadband LFP', fontsize=26)
 plt.subplots_adjust(hspace=0.7, wspace=0.25, top=0.82, right=right_margin, left=0.1)
 
 # we're plotting the same thing in each subplot, so only grab labels for one plot
@@ -134,13 +138,16 @@ if pvalues_corr is not None:
         elif pvalues_corr[t] < 0.05:
             ax_curr.axvspan(trial_time[t] - time_diff[t - 1] / 2, trial_time[t] + time_diff[t] / 2, color='red',
                     alpha=0.3)
-ax_curr.axvspan(0.3, 0.6, color='grey', alpha=0.3)
+# ax_curr.axvspan(0.3, 0.6, color='grey', alpha=0.3)
 ax_curr.axvline(0, linestyle='--', c='black')
+chance_performance = np.ones((len(trial_time),1)) * 0.5
+ax_curr.plot(trial_time, chance_performance, linestyle='--', color='purple')
 # ax_curr.set_title(signal_names[ind])
 
-ax_curr.set_xlabel('Time (s)')
+ax_curr.set_xlabel('Time (s)', fontsize=20)
+ax_curr.set_ylabel('Decoding Accuracy', fontsize=20)
 ax_curr.set_xticks(time_ticks, time_tick_labels)
-
+ax_curr.set_xlim([np.min(trial_time), np.max(trial_time)])
 ## plt.suptitle(f'Mean Activity of SU \n {subject} - session {session} \n {extra_string}')
 #plt.subplots_adjust(hspace=0.7, wspace=0.25, top=0.82, right=right_margin, left=0.1)
 
@@ -148,22 +155,64 @@ ax_curr.set_xticks(time_ticks, time_tick_labels)
 # lines, labels = ax.get_legend_handles_labels()
 # fig.legend(lines, labels, loc='right', ncol=1)
 plt.savefig(f'{os.pardir}/results/LDA_{test_subject}_{test_session}_{electrode_selection}.svg')
-
+plt.show()
 np.savez(f'{os.pardir}/results/LDA_{test_subject}_{test_session}_{electrode_selection}_loadings.npz',
          np.array(lda_loadings), trial_time, microwire_names)
 
 lda_loadings = np.squeeze(np.array(lda_loadings)).T
-fig, ax = plt.subplots()
-im = ax.imshow(lda_loadings)
+if electrode_selection == 'all':
+    fig, ax = plt.subplots(figsize=(7, 5))
+    ax.set_ylabel('Electrode Locations', fontsize=18)
+    label_textsize = 6
+else:
+    fig, ax = plt.subplots(figsize=(7,4))
+    ax.set_ylabel('Electrode \n Locations', fontsize=18)
+    label_textsize = 8
+
+# find minimum of minima & maximum of maxima
+vmin = np.min(lda_loadings)
+vmax = np.max(lda_loadings)
+im = ax.imshow(lda_loadings, vmin=vmin, vmax=vmax, cmap='viridis')
 
 # Show all ticks and label them with the respective list entries
 lda_time_ticks = np.array([ind for ind,i in enumerate(trial_time) if i in time_ticks])
 ax.set_xticks(lda_time_ticks, time_tick_labels[1:-1])
-ax.set_yticks(np.arange(len(microwire_names)), labels=microwire_names)
 
-ax.set_title(f'LDA weights on {electrode_selection} electrodes, {event_lock}-lock')
+import pandas as pd
+anat_csv = f"{os.pardir}/data/{test_subject}/{test_subject}_Elec_Notes.xlsx"
+# anat_csv = "/path/to/csv/file/with/mni/coordinates/coords_mni.csv"
+# raw = read_raw_fif("/path/to/seeg/sub-009_preproc_ieeg.fif")  # preprocessed seeg data
+
+# epoch at start of audio playback
+# epochs = mne.Epochs(raw, event_id={"wav_playback"}, detrend=1, baseline=None)
+
+# load the coordinate values
+coords = pd.read_excel(anat_csv)
+# coords = coords[["Electrode", "MNI_1", "MNI_2", "MNI_3", "Loc Meeting"]]
+
+localization_dict = {}
+feature_key_values = [(coords.loc[ind, "Electrode"], coords.loc[ind, "Loc Meeting"]) for ind in coords.index.values]
+localization_dict.update(feature_key_values)
+localization_names = [localization_dict[wire] if not wire.startswith('m') else 'u' +
+                                                                               localization_dict[wire[1:4] +'1']
+                      for ind, wire in enumerate(microwire_names)]
+
+ax.set_yticks(np.arange(len(microwire_names)), labels=localization_names, fontsize=label_textsize)
+
+ax.set_xlabel('Time(s)', fontsize=18)
+
+# ax.set_title(f'LDA weights on {electrode_selection} electrodes, {event_lock}-lock')
+ax.set_title(f'LDA weights on \n {electrode_selection} electrodes', fontsize=26)
 # Rotate the tick labels and set their alignment.
 plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
          rotation_mode="anchor")
+plt.tight_layout()
+
+# add space for colour bar
+fig.subplots_adjust(right=0.88)
+cbar_ax = fig.add_axes([0.91, 0.15, 0.04, 0.5])
+fig.colorbar(im, cax=cbar_ax)
+
 plt.savefig(f'{os.pardir}/results/LDA_{test_subject}_{test_session}_{electrode_selection}_loadings.svg')
 plt.show()
+print('hmm')
